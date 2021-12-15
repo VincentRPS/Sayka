113c26912fe7ef092ccb6fb950e4e3d48cea4ff3
Vincent <vincentbusiness55@gmail.com>
2021-12-15 20:48:32 +0800

    remove @discordjs/builders

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,10 +1,1 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('ping, Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
-};
+// TODO: Redo
